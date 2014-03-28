34ce902f5d8788fc3e46667fea6bc227723de005
aurigadl <aurigadl@gmail.com>
2014-03-28 11:43:20 -0500

    Nuevos enlaces en la bandeja
    Former-commit-id: c0e79ad2de577446fbf4e14b18c03725f5ab5c30

--- index_frames.php
+++ index_frames.php
@@ -5,6 +5,9 @@
   if (!$_SESSION['dependencia'])
       header ("Location: $ruta_raiz/cerrar_session.php");
 
+  include_once "$ruta_raiz/include/db/ConnectionHandler.php";
+  $db = new ConnectionHandler("$ruta_raiz");
+
   $krd            = $_SESSION["krd"];
   $dependencia    = $_SESSION["dependencia"];
   $usua_doc       = $_SESSION["usua_doc"];
@@ -14,39 +17,101 @@
   $tip3img        = $_SESSION["tip3img"];
   $ESTILOS_PATH   = $_SESSION["ESTILOS_PATH"];
   $nombUser       = $_SESSION["usua_nomb"];
+  $phpsession     = session_name()."=".session_id();
   $fechah         = date("Ymdhms");
   $ruta_raiz      = ".";
+  $enlace         = "href=\"cuerpo.php?<?=$phpsession?>&adodb_next_page=1&fechah=";
+  $enlace1        = "href=\"cuerpoAgenda.php?<?=$phpsession?>&agendado=1&fechah=";
+  $enlace2        = "href=\"cuerpoAgenda.php?<?=$phpsession?>&agendado=2&fechah=";
+  $enlace3        = "href=\"cuerpoinf.php?<?=$phpsession?>&<?=mostrar_opc_envio=1&orderNo=2&fechaf=$fechah&
+                     carpeta=8&nomcarpeta=Informados&orderTipo=desc&adodb_next_page=1\"";
+  $enlace4        = "href=\"tx/cuerpoInfConjunto.php?$phpsession&mostrar_opc_envio=1&orderNo=2&fechaf=$fechah&
+                     carpeta=66&nomcarpeta=Informados&orderTipo=desc&adodb_next_page=1\"";
+  $enlace5        = "href=\"cuerpoTx.php?<?=$phpsession?>&fechah=";
 
-  include_once "$ruta_raiz/include/db/ConnectionHandler.php";
-  $db = new ConnectionHandler("$ruta_raiz");
+  $sqlFechaHoy    = $db->conn->DBTimeStamp(time());
 
-  // BUSCA LAS CARPETAS PERSONALES DE CADA USUARIO Y LAS COLOCA contando el numero de documentos en cada carpeta.
-  $isql ="SELECT DISTINCT CODI_CARP,DESC_CARP,NOMB_CARP
-          FROM CARPETA_PER
-          WHERE USUA_CODI=$codusuario AND DEPE_CODI=$dependencia ORDER BY CODI_CARP";
-
-  $rs=$db->query($isql);
+	// Esta consulta selecciona las carpetas como Devueltos,  Entrada ,salida
+  $isql = "SELECT CARP_CODI,CARP_DESC FROM CARPETA ORDER BY CARP_CODI";
+	$rs   = $db->query($isql);
 
   while(!$rs->EOF){
-    if($data=="")   $data = "NULL";
-    $data    = trim($rs->fields["NOMB_CARP"]);
-    $numdata = trim($rs->fields["CODI_CARP"]);
-    $detalle = trim($rs->fields["DESC_CARP"]);
-    $data    = trim($rs->fields["NOMB_CARP"]) ;
+    $numdata    = trim($rs->fields["CARP_CODI"]);
+    $sqlCarpDep = "SELECT
+                    SGD_CARP_DESCR
+                  FROM
+                    SGD_CARP_DESCRIPCION
+                  WHERE
+                    SGD_CARP_DEPECODI    = $dependencia
+                    AND SGD_CARP_TIPORAD = $numdata";
 
-    $isql    = "SELECT COUNT(1) AS CONTADOR
-                FROM RADICADO
-                WHERE CARP_PER=1 AND CARP_CODI=$numdata
-                AND RADI_DEPE_ACTU=$dependencia AND RADI_USUA_ACTU=$codusuario";
+    $rsCarpDesc         = $db->query($sqlCarpDep);
+    $descripcionCarpeta = $rsCarpDesc->fields["SGD_CARP_DESCR"];
+    $desccarpt          = $rs->fields["CARP_DESC"];
 
-    $rs1     = $db->query($isql);
-    $numerot = $rs1->fields["CONTADOR"];
+    $data       = (empty($descripcionCarpeta))? trim($desccarpt) : $descripcionCarpeta;
+    $link1      = $enlace."$fechah&nomcarpeta=$data&carpeta=$numdata&tipo_carpt=0&adodb_next_page=1\"";
+    $link1show .= "<li><a $link1 target=\"mainFrame\" >$desccarpt</a></li>";
 
-    $links.="<a href=\"cuerpo.php?".$phpsession."&.\"fechah=".$fechah."&tipo_carp=1&carpeta=".$numdata."&nomcarpeta=".$data."(Personal) alt=\"".$detalle." title=\"".$detalle." class=\"menu_princ\" target=\"mainFram\">".$data." ". $numerot;
-   echo $links;
     $rs->MoveNext();
   }
 
+  //Agendado
+  $isql        =" SELECT COUNT(1) AS CONTADOR
+                  FROM SGD_AGEN_AGENDADOS agen
+                  WHERE usua_doc='$usua_doc'
+                  and agen.SGD_AGEN_ACTIVO=1
+                  and (agen.SGD_AGEN_FECHPLAZO >= $sqlFechaHoy )";
+
+  $rs       = $db->query($isql);
+  $num_exp  = $rs->fields["CONTADOR"];
+  $data     = "Agendados no vencidos";
+  $link2    = $enlace1."$fechah&nomcarpeta=$data&tipo_carpt=0\"";
+  $link2show= "<li><a $link2 target=\"mainFrame\" >Agendado($num_exp)</a></li>";
+
+
+
+  //Agendado  Vencido
+  $isql="SELECT COUNT(1) AS CONTADOR
+          FROM SGD_AGEN_AGENDADOS AGEN
+          WHERE  USUA_DOC='$usua_doc'
+          and agen.SGD_AGEN_ACTIVO=1
+          and (agen.SGD_AGEN_FECHPLAZO <= $sqlFechaHoy)";
+  $rs=$db->query($isql);
+  $num_exp  = $rs->fields["CONTADOR"];
+	$data     ="Agendados vencidos";
+  $link3    = $enlace2."$fechah&nomcarpeta=$data&&tipo_carpt=0&adodb_next_page=1\"";
+  $link3show= "<li><a $link3 target=\"mainFrame\" >Agendado Vencido (<font color='#990000'>$num_exp</font>)</a></li>";
+
+
+  //Informados
+  $isql   =" SELECT COUNT(1) AS CONTADOR
+               FROM INFORMADOS
+             WHERE DEPE_CODI=$dependencia
+              and usua_codi=$codusuario
+              and info_conjunto=0";
+
+  $rs1     = $db->query($isql);
+  $numerot = ($rs1)? $rs1->fields["CONTADOR"] : 0;
+  $link4show= "<li><a $enlace3 target=\"mainFrame\" >Informados ($numerot)</a></li>";
+
+  //Tramite conjunto
+  $isql="SELECT COUNT(1) AS CONTADOR
+         FROM INFORMADOS
+         WHERE DEPE_CODI=$dependencia
+          and usua_codi=$codusuario
+          and info_conjunto>=1 ";
+  $rs1=$db->query($isql);
+
+  $numerot = ($rs1)? $rs1->fields["CONTADOR"] : 0;
+  if($numerot>=1){
+    $link5show= "<li><a $enlace4 target=\"mainFrame\"> Tramite Conjunto ($numerot)</a></li>";
+  }
+
+  //Ultimas transacciones del usuario
+  $data     ="Ultimas Transacciones del Usuario";
+  $link6    = $enlace5."$fechah&nomcarpeta=$data&tipo_carpt=0\"";
+  $link6show= "<li><a $link6 target=\"mainFrame\">Transacciones</a></li>";
 ?>
 
 <html lang="es">
@@ -66,60 +131,9 @@
         <link href="./estilos/siim_temp.css" rel="stylesheet">
         <script type="text/javascript" src="<?=$ruta_raiz?>/js/jquery.min.js"></script>
         <script type="text/javascript" src="<?=$ruta_raiz?>/js/bootstrap.js"></script>
-        <!-- inicio script que crea la sesion y la cierra para el carro de compras-->
-
-        <script language="javascript">
-            function returnKrdF_top(){
-                return '<?=$krd?>';
-            };
-
-            function nueva(){
-                open('plantillas.php?<?=session_name()."=".session_id()?>', 'Sizewindow', 'width=800,height=600,scrollbars=yes,toolbar=no')
-            }
-
-            function cerrar_session() {
-              if (confirm('Seguro de cerrar sesion ?')){
-                <?$fechah = date("Ymdhms"); ?>
-                url="login.php?adios=chao";document.form_cerrar.submit();
-                url = 'login.php?<?= session_name()."=".session_id()."&fechah=$fechah"?>';
-                window.location.href=url;
-              }
-            }
-
-            function MM_swapImgRestore(){
-                var i,x,a=document.MM_sr; for(i=0;a&&i
-                <a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-            }
-
-            function MM_preloadImages(){
-                var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-                var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i
-                <a.length; i++)
-                if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-            }
-
-            function MM_findObj(n, d){
-                var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-                d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-                if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i
-                <d.forms.length;i++) x=d.forms[i][n];
-                for(i=0;!x&&d.layers&&i
-                <d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-                if(!x && d.getElementById) x=d.getElementById(n); return x;
-            }
-
-            function MM_swapImage(){
-                var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i
-                <(a.length-2);i+=3)
-                if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-            }
-
-        </script>
-        <!-- FIN    Script que crea la sesion y la cierra para el carro de compras-->
-        <script type="text/javascript" src="<?=$ruta_raiz?>/js/ajaxSessionRads.js"></script>
     </head>
 
-    <body topmargin="0" leftmargin="0" onLoad="MM_preloadImages('');MM_preloadImages('');MM_preloadImages('');MM_preloadImages('')">
+    <body>
       <div id="wrapper">
         <!-- Sidebar -->
           <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -142,13 +156,14 @@
                   <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"> Bandejas <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
+                      <?=$link1show?>
+                      <?=$link4show?>
+                      <?=$link5show?>
                       <li class="divider"></li>
-                      <li><a href="#">Separated link</a></li>
+                      <?=$link6show?>
                       <li class="divider"></li>
-                      <li><a href="#">One more separated link</a></li>
+                      <?=$link2show?>
+                      <?=$link3show?>
                     </ul>
                   </li>
                 </ul>
