6589a9294f659f8580be061566b7bee0d644087a
jlosada <jlosada@gmail.com>
2014-04-22 18:39:18 -0500

    Arreglo de expedientes y subexpedientes asi como vistas de anexos
    Former-commit-id: 2438f7c8b974878d12c12b91eb0fc31cd9b19a1c

--- expediente/expedienteTree.php
+++ expediente/expedienteTree.php
@@ -2,66 +2,66 @@
 <article class="col-sm-12 col-md-12" align=left>
 <!--widget content -->
 <div class="widget-body">
-	<div class="tree smart-form fa-folder-close">
+<div class="tree smart-form fa-folder-close">
+	<ul>
+	<li>
+		<span class="alert-success"><i class="fa fa-folder-close"></i> <?=$numExpediente?></span>
 		<ul>
-		<li>
-			<span class="alert-success"><i class="fa fa-folder-close"></i> <?=$numExpediente?></span>
-			<ul>
+		<?
+		foreach($datosExp as $key2 => $value){
+		?>
+		<li  style="display:none">
+			<span>
 			<?
-			foreach($datosExp as $key2 => $value){
-			?>
-			<li  style="display:none">
-				<span>
-				<?
-				 $sExp = $value["SEXPEDIENTES"];
-				 
-				 if($sExp){
-				 $SExps = "<table><TR><TD COLSPAN=5></small>Expedientes Adicionales :</small><TD></TR>";
-				 foreach($sExp as $valueExpedientes){
-				    $SExps .= "<tr><td><small>".$valueExpedientes["NUMERO"]."&nbsp; </small> </td><td> <small>".$valueExpedientes["PARAM1"]."&nbsp; </small></td><td><small>".$valueExpedientes["PARAM2"]."</small></td></tr>";
-				 }
-				 $SExps .= "</table>";
-				 }
-				$numRadicado = $value["NUM_RADICADO"];
-				$fechaRad = "<a href='verradicado.php?verrad=$numRadicado&".session_name()."=".session_id()."&nomcarpeta=".$nomcarpeta."#tabs-d'>".$value["FECHA_RADICADO"] . "</a>";
-				echo "<TABLE  WIDTH='1050'><TR><TD WIDTH=30><i class='icon-leaf'></i> </TD>
-				<TD width=140 align=left>";
-				if($valueAnexos["PATH_RADICADO"]) echo "<a href='$ruta_raiz/".$value["PATH_RADICADO"]."' target='".date("ymdhis")."'>";
-				echo $numRadicado;
-				if($valueAnexos["PATH_RADICADO"]) echo "</a>" ;
-				echo "</TD>
-				<TD width=140>$fechaRad</TD><TD width=120>".$value["TIPO_DRADICADO"]."</TD><TD width=450>".$value["ASUNTO_RADICADO"]."</TD>
-				<TD width=350>$SExps</TD></TR>
-				</TABLE>"; ?></span>
+				$sExp = $value["SEXPEDIENTES"];
+				
+				if($sExp){
+				$SExps = "<table><TR><TD COLSPAN=5></small><b>Expedientes Adicionales :</b></small><TD></TR>";
+				foreach($sExp as $valueExpedientes){
+					$SExps .= "<tr><td><small>".$valueExpedientes["NUMERO"]."&nbsp; </small> </td><td> <small>".$valueExpedientes["PARAM1"]."&nbsp; </small></td><td><small>".$valueExpedientes["PARAM2"]."</small></td></tr>";
+				}
+				$SExps .= "</table>";
+				}
+			$numRadicado = $value["NUM_RADICADO"];
+			$fechaRad = "<a href='verradicado.php?verrad=$numRadicado&".session_name()."=".session_id()."&nomcarpeta=".$nomcarpeta."#tabs-d'>".$value["FECHA_RADICADO"] . "</a>";
+			echo "<TABLE  WIDTH='1050'><TR><TD WIDTH=30><i class='icon-leaf'></i> </TD>
+			<TD width=140 align=left>";
+			if($valueAnexos["PATH_RADICADO"]) echo "<a href='$ruta_raiz/".$value["PATH_RADICADO"]."' target='".date("ymdhis")."'>";
+			echo $numRadicado;
+			if($valueAnexos["PATH_RADICADO"]) echo "</a>" ;
+			echo "</TD>
+			<TD width=140>$fechaRad</TD><TD width=120>".$value["TIPO_DRADICADO"]."</TD><TD width=450>".$value["ASUNTO_RADICADO"]."</TD>
+			<TD width=350>$SExps</TD></TR>
+			</TABLE>"; ?></span>
 
-				<ul>
-        <?
-            $carpetaDep = intval(substr($value["NUM_RADICADO"],4,$digitosDependencia));
-            $rutaAnexos = "".substr($value["NUM_RADICADO"],0,4). "/$carpetaDep/docs/";
-            $anexos = $value["ANEXOS"];
-            if($anexos){
-            foreach($anexos as $valueAnexos){
-          ?>
-          <li style="display:none">
-            <?=$valueAnexos["ANEX_NUMERO"]?> - <?=$valueAnexos["RADI_SALIDA"]?>
-            <? if($valueAnexos["ANEX_PATH"]) {?><a href='<?=$ruta_raiz."/bodega/$rutaAnexos".$valueAnexos["ANEX_PATH"]?>' target='<?=date("ymdhis")?>'> <? } ?>
-            - <?=$valueAnexos["DESCRIPCION"]?>
-            <? if($valueAnexos["ANEX_PATH"]) {?> </a> <? } ?>
-          </li>
-          <?
-            }
-          }
-          ?>
-          </ul>
+			<ul>
+		<?
+				$carpetaDep = intval(substr($value["NUM_RADICADO"],4,$digitosDependencia));
+				$rutaAnexos = "".substr($value["NUM_RADICADO"],0,4). "/$carpetaDep/docs/";
+				$anexos = $value["ANEXOS"];
+				// if($anexos){
+				foreach($anexos as $valueAnexos){
+			?>
+			<li style="display:none">
+				<?=$valueAnexos["ANEX_NUMERO"]?> - <?=$valueAnexos["RADI_SALIDA"]?>
+				<? if($valueAnexos["ANEX_PATH"]) {?><a href='<?=$ruta_raiz."/bodega/$rutaAnexos".$valueAnexos["ANEX_PATH"]?>' target='<?=date("ymdhis")?>'> <? } ?>
+				- <?=$valueAnexos["DESCRIPCION"]?>
+				<? if($valueAnexos["ANEX_PATH"]) {?> </a> <? } ?>
 			</li>
 			<?
-			}
+				}
+			//}
+			?>
+			</ul>
+	</li>
+	<?
+	}
 			
 		$url = "http://200.118.122.176:5380/py/pyforms/seleccion_predios/get_predios_list?expediente=$numExpediente";
 		$predios = file_get_contents($url);
 		$predios = str_replace('"',"'",$predios);
 		$arrPredios = json_decode($predios);
-		if($arrPredios){
+		if(is_array($arrPredios)){
 			?>
 			<li  style="display:none">
 				<span  class="alert-success">
