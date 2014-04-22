41135d44112e8208eb648cec93348275bc81f948
jlosada <jlosada@gmail.com>
2014-04-22 16:24:54 -0500

    Se conecta con los predios y el gis...
    Former-commit-id: 30e645237a1da62fab4b470e29e64dbd7d025d73

--- gis/verGis.php
+++ gis/verGis.php
@@ -1,1 +1,6 @@
-<iframe src="http://162.243.69.213:8081/mtvSIG/?numeroExpediente=<?=$numExpediente?>" width="100%" height="80%"></iframe>
+<? 
+  $prediosGis = "";
+  if($prediosExp) $prediosGis = "&prediosExp=$prediosExp";
+  $url = "http://162.243.69.213:8081/mtvSIG/?numeroExpediente=$numExpediente".$prediosGis;
+?>
+<iframe src="<?=$url?>" width="100%" height="80%"></iframe>
