ee1169d370ddfe1d875559c50055d4ca8777dd05
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2014-05-21 16:27:14 -0500

    Added config param for servidorGis
    Former-commit-id: 7ff8fada27aa6587a66ce30b188ea1c5b5e6c835

--- gis/verGis.php
+++ gis/verGis.php
@@ -1,6 +1,8 @@
 <? 
+  include_once('../config.php')
+
   $prediosGis = "";
   if($prediosExp) $prediosGis = "&prediosExp=$prediosExp";
-  $url = "http://162.243.69.213:8081/mtvSIG/?numeroExpediente=$numExpediente".$prediosGis;
+  $url = $servidorGis . "?numeroExpediente=$numExpediente".$prediosGis;
 ?>
 <iframe src="<?=$url?>" width="100%" height="80%"></iframe>
