718b804620a3e66e2a81f57ffb229c2937326656
aurigadl <aurigadl@gmail.com>
2014-08-01 17:07:43 -0500

    Se elimina un debug activo
    Former-commit-id: c035543504ca2b3239afe76885d35d08bd55ab17

--- Administracion/tbasicas/adm_mensajeRapido.php
+++ Administracion/tbasicas/adm_mensajeRapido.php
@@ -33,8 +33,6 @@ session_start();
     include_once    ("$ruta_raiz/include/db/ConnectionHandler.php");
 
     $db = new ConnectionHandler($ruta_raiz);
-    $db->conn->debug = true;
-
 
     if (isset($_POST['btn_accion'])){
 
