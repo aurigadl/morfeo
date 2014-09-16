5b27d8a9c6cd50993653c25216a7cd55600bdc7a
jlosada <jlosada@gmail.com>
2014-09-16 00:19:19 -0500

    Se arregla gran parte de liquidacion y se inicia con el trabajo de ajuntar documentos
    Former-commit-id: 2eaced7fe4bc17311a00b7c437066d1ec1d38bde

--- mtvLiquidacion/radPrint.php
+++ mtvLiquidacion/radPrint.php
@@ -53,7 +53,7 @@ del traslado de la obligación urbanística.
  <?=$pData?>
 </div>
 <br><br>
-
+<small>
 <b>Documentos Ajuntos a esta solicitud.</b><br>
 <label name=file1 id=file1></label>
 <label name=file2 id=file2></label>
@@ -62,6 +62,7 @@ del traslado de la obligación urbanística.
 <label name=file5 id=file5></label>
 <label name=file6 id=file6></label>
 <label name=file7 id=file7></label>
+</small>
 <br>
 <br>
 Recuerde:
@@ -75,10 +76,10 @@ Metrovivienda organiza, garantiza y articula una oferta y una demanda de viviend
   document.getElementById("pData").innerHTML =pData;
   document.getElementById("fname").innerHTML = window.opener.fname.value;
   document.getElementById("faddress").innerHTML = window.opener.address.value + "<br>";
-  if(window.opener.file1.value) document.getElementById("file1").innerHTML = window.opener.file1.value + " (Seleccionar Certificado de Tradici&oacute;n y Libertad.)<br>";
+  if(window.opener.file1.value) document.getElementById("file1").innerHTML = window.opener.file1.value + " (Certificado de Tradici&oacute;n y Libertad.)<br>";
   if(window.opener.file2.value) document.getElementById("file2").innerHTML = window.opener.file2.value + " (Certificado Catastral vigente)<br>";
-  if(window.opener.file3.value) document.getElementById("file3").innerHTML = window.opener.file3.value + " (Certificado de Existencia y Representación <br> (En caso de ser Persona juridica el titular de la licencia)<br>";
-  if(window.opener.file4.value) document.getElementById("file4").innerHTML = window.opener.file4.value + " (Cédula de Ciudadanía del Titular de la licencia (En caso de ser persona natural el titular de la licencia))<br>";
+  if(window.opener.file3.value) document.getElementById("file3").innerHTML = window.opener.file3.value + " (Certificado de Existencia y Representación )<br>";
+  if(window.opener.file4.value) document.getElementById("file4").innerHTML = window.opener.file4.value + " (Cédula de Ciudadanía del Titular de la licencia.)<br>";
   if(window.opener.file5.value) document.getElementById("file5").innerHTML = window.opener.file5.value + " (Certificación de la Curaduría Urbana)<br>";
   if(window.opener.file6.value) document.getElementById("file6").innerHTML = window.opener.file6.value + " (Poder)<br>";
   window.print();
