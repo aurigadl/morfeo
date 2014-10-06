4ec1bc74e195df161f64797afc9ccb7429ef2b8b
Cesar Buelvas <cejebuto@gmail.com>
2014-10-06 17:36:31 -0500

    Generador png de codigos de barras
    Former-commit-id: 0710838a805d7bc68998a9ac51b93998f0aaaa64

---
+++ respuestaRapida/gencodebar/html/BCGcode128.php
@@ -0,0 +1,65 @@
+<?php
+define('IN_CB', true);
+include('include/header.php');
+
+//$default_value['start'] = 'A';
+$start = 'NULL';
+//$start = isset($_POST['start']) ? $_POST['start'] : $default_value['start'];
+registerImageKey('start', $start);
+registerImageKey('code', 'BCGcode128');
+
+$vals = array();
+for($i = 0; $i <= 127; $i++) {
+    $vals[] = '%' . sprintf('%02X', $i);
+}
+$characters = array(
+    'NUL', 'SOH', 'STX', 'ETX', 'EOT', 'ENQ', 'ACK', 'BEL', 'BS', 'TAB', 'LF', 'VT', 'FF', 'CR', 'SO', 'SI', 'DLE', 'DC1', 'DC2', 'DC3', 'DC4', 'NAK', 'SYN', 'ETB', 'CAN', 'EM', 'SUB', 'ESC', 'FS', 'GS', 'RS', 'US',
+    '&nbsp;', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?',
+    '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_',
+    '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 'DEL'
+);
+?>
+<?php /* ?>
+<ul id="specificOptions">
+    <li class="option">
+        <div class="title">
+            <label for="start">Starts with</label>
+        </div>
+        <div class="value">
+            <?php echo getSelectHtml('start', $start, array('NULL' => 'Auto', 'A' => 'Code 128-A', 'B' => 'Code 128-B', 'C' => 'Code 128-C')); ?>
+        </div>
+    </li>
+</ul>
+<div id="validCharacters">
+    <h3>Valid Characters</h3>
+    <?php $c = count($characters); for ($i = 0; $i < $c; $i++) { echo getButton($characters[$i], $vals[$i]); } ?>
+</div>
+
+<div id="explanation">
+    <h3>Explanation</h3>
+    <ul>
+        <li>Code 128 is a high-density alphanumeric symbology.</li>
+        <li>Used extensively worldwide.</li>
+        <li>Code 128 is designed to encode 128 full ASCII characters.</li>
+        <li>The symbology includes a checksum digit.</li>
+        <li>Code 128A handles capital letters<br />Code 128B handles capital letters and lowercase<br />Code 128C handles group of 2 numbers</li>
+        <li>Your browser may not be able to write the special characters (NUL, SOH, etc.) but you can write them with the code.</li>
+    </ul>
+</div>
+<?php */ ?>
+
+            <div class="output">
+                <section class="output">
+                    <h3>Radicado No 20149000002761 </h3>
+                    <?php 
+                        $finalRequest = '';foreach (getImageKeys() as $key => $value) {$finalRequest .= '&' . $key . '=' . urlencode($value);}if (strlen($finalRequest) > 0) {$finalRequest[0] = '?';} 
+                    ?>
+                    <div id="imageOutput">
+                        <?php if ($imageKeys['text'] !== '') { ?><img src="image.php<?php echo $finalRequest; ?>" alt="Radicado" /><?php }
+                        else { ?>Fill the form to generate a barcode.<?php } ?>
+                    </div>
+                </section>
+            </div>
+        </form>
+     </body>
+</html>
---
+++ respuestaRapida/gencodebar/html/include/header.php
@@ -0,0 +1,131 @@
+<?php
+if (!defined('IN_CB')) { die('You are not allowed to access to this page.'); }
+
+if (version_compare(phpversion(), '5.0.0', '>=') !== true) {
+    exit('Sorry, but you have to run this script with PHP5... You currently have the version <b>' . phpversion() . '</b>.');
+}
+
+if (!function_exists('imagecreate')) {
+    exit('Sorry, make sure you have the GD extension installed before running this script.');
+}
+
+include_once('function.php');
+
+// FileName & Extension
+$system_temp_array = explode('/', $_SERVER['PHP_SELF']);
+$filename = $system_temp_array[count($system_temp_array) - 1];
+$system_temp_array2 = explode('.', $filename);
+$availableBarcodes = listBarcodes();
+$barcodeName = findValueFromKey($availableBarcodes, $filename);
+$code = $system_temp_array2[0];
+
+// Check if the code is valid
+if (file_exists('config' . DIRECTORY_SEPARATOR . $code . '.php')) {
+    include_once('config' . DIRECTORY_SEPARATOR . $code . '.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $barcodeName; ?> - Barcode Generator</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link type="text/css" rel="stylesheet" href="style.css" />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <script src="jquery-1.7.2.min.js"></script>
+        <script src="barcode.js"></script>
+    </head>
+    <body class="<?php echo $code; ?>">
+
+<?php
+$default_value = array();
+$default_value['filetype'] = 'PNG';
+$default_value['dpi'] = 72;
+$default_value['scale'] = isset($defaultScale) ? $defaultScale : 1;
+$default_value['rotation'] = 0;
+$default_value['font_family'] ='0';
+//$default_value['font_size'] = 8;
+$default_value['text'] = '';
+$default_value['a1'] = '';
+$default_value['a2'] = '';
+$default_value['a3'] = '';
+
+/*$filetype = isset($_POST['filetype']) ? $_POST['filetype'] : $default_value['filetype'];
+$dpi = isset($_POST['dpi']) ? $_POST['dpi'] : $default_value['dpi'];
+$scale = intval(isset($_POST['scale']) ? $_POST['scale'] : $default_value['scale']);
+$rotation = intval(isset($_POST['rotation']) ? $_POST['rotation'] : $default_value['rotation']);
+$font_family = isset($_POST['font_family']) ? $_POST['font_family'] : $default_value['font_family'];
+$font_size = intval(isset($_POST['font_size']) ? $_POST['font_size'] : $default_value['font_size']);
+$text = isset($_POST['text']) ? $_POST['text'] : $default_value['text']; */
+
+$filetype = 'PNG';
+$dpi = 72;
+$scale = 1;
+$rotation = 0;
+$font_family = '0';
+$font_size = 8;
+$text = '20149000002761';
+
+registerImageKey('filetype', $filetype);
+registerImageKey('dpi', $dpi);
+registerImageKey('scale', $scale);
+registerImageKey('rotation', $rotation);
+registerImageKey('font_family', $font_family);
+registerImageKey('font_size', $font_size);
+registerImageKey('text', stripslashes($text));
+
+// Text in form is different than text sent to the image
+$text = convertText($text);
+?>
+
+
+<?php /* ?>
+
+<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+    <h1>Barcode Generator</h1>
+    <h2><?php echo $barcodeName; ?></h2>
+    <div class="configurations">
+        <section class="configurations">
+            <h3>Configurations</h3>
+            <table>
+                <colgroup>
+                    <col class="col1" />
+                    <col class="col2" />
+                </colgroup>
+                <tbody>
+                    <tr>
+                        <td><label for="filetype">File type</label></td>
+                        <td><?php echo getSelectHtml('filetype', $filetype, array('PNG' => 'PNG - Portable Network Graphics', 'JPEG' => 'JPEG - Joint Photographic Experts Group', 'GIF' => 'GIF - Graphics Interchange Format')); ?></td>
+                    </tr>
+                    <tr>
+                        <td><label for="dpi">DPI</label></td>
+                        <td><?php echo getInputTextHtml('dpi', $dpi, array('type' => 'number', 'min' => 72, 'max' => 300, 'required' => 'required')); ?> <span id="dpiUnavailable">DPI is available only for PNG and JPEG.</span></td>
+                    </tr>
+<?php
+if (isset($baseClassFile) && file_exists('include' . DIRECTORY_SEPARATOR . $baseClassFile)) {
+    include_once('include' . DIRECTORY_SEPARATOR . $baseClassFile);
+}
+?>
+                    <tr>
+                        <td><label for="scale">Scale</label></td>
+                        <td><?php echo getInputTextHtml('scale', $scale, array('type' => 'number', 'min' => 1, 'max' => 4, 'required' => 'required')); ?></td>
+                    </tr>
+                    <tr>
+                        <td><label for="rotation">Rotation</label></td>
+                        <td><?php echo getSelectHtml('rotation', $rotation, array(0 => 'No rotation', 90 => '90&deg; clockwise', 180 => '180&deg; clockwise', 270 => '270&deg; clockwise')); ?></td>
+                    </tr>
+                    <tr>
+                        <td><label for="font_family">Fuente</label></td>
+                        <td><select size="1" id="font_family" name="font_family"><option value="0" selected="selected">No Label</option></select></td>
+                    </tr>
+                    <tr>
+                        <td><label for="text">Data</label></td>
+                        <td>
+                            <div class="generate" style="float: left"><?php echo getInputTextHtml('text', $text, array('type' => 'text', 'required' => 'required')); ?> <input type="submit" value="Generate!" /></div>
+                            <div class="possiblechars" style="float: right; position: relative;"><a href="#" class="info characters"><img src="info.gif" alt="Help" /></a></div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </div>
+    <?php */ ?>
