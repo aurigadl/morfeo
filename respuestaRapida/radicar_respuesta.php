74ff3ed26a889ee46b64557893f675fe772f20b6
Cesar Buelvas <cejebuto@gmail.com>
2014-10-07 12:18:18 -0500

    correcciones codigo de barra
    Former-commit-id: 070359024cae52e9811bd4321d035e2d4d0eeebe

--- respuestaRapida/radicar_respuesta.php
+++ respuestaRapida/radicar_respuesta.php
@@ -595,7 +595,7 @@ $pdf->setFontSubsetting(true);
 $pdf->AddPage();
 
 // CODE 128 AUTO
- $pdf->write1DBarcode($nurad, 'C128', '', '', '', 15, 0.4, $style, 'L');
+ $pdf->write1DBarcode($nurad, 'C128', '', '300', '', 18, 0.4, $style, '');
 
 #$pdf->setXY(93,272); 
 #$pdf->write1DBarcode("074001726000003006652985", 'C39', '', '', 90, 10, 0.4, '', 'N'); 
